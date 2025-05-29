b39ceba5c4d713a5838eb2e18f2ec41b61f852a0
kenjanjan <kjliloc@hcdc.edu.ph>
2025-05-29 18:30:12 +0800

    Add Admin Dashboard and Layout components

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Admin | Dashboard",
+  description: "Your personal dashboard",
+};
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminDashboard
