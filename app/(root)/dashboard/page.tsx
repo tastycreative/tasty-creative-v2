628e95e8c72ff7825e47c678566b81a368d14deb
kenjanjan <kjliloc@hcdc.edu.ph>
2025-05-29 18:07:37 +0800

    Add initial Dashboard component with metadata

---
+++ app/(root)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Dashboard",
+  description: "Your personal dashboard",
+};
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
