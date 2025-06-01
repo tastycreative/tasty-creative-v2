20373b48e49a3c3e500dc2da8109f683b4faa831
kenjanjan <kjliloc@hcdc.edu.ph>
2025-06-01 19:40:45 +0800

    feat: update session check to redirect only for unauthenticated users

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
 }) {
   const session = await auth();
 
-  if (session?.user?.role === "GUEST" || !session) {
+  if (!session) {
     redirect("/");
   }
 
