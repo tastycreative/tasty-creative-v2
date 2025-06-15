b046cfca400a270e968acaf5131ec9795ee42422
kenjanjan <kjliloc@hcdc.edu.ph>
2025-06-15 16:09:01 +0800

    Refactor GET request handler to remove redundant parameter checks for 'path' and 'url'

--- app/api/be/proxy/route.ts
+++ app/api/be/proxy/route.ts
@@ -5,18 +5,9 @@ export async function GET(req: NextRequest) {
   const encodedPath = req.nextUrl.searchParams.get("path");
   const encodedUrl = req.nextUrl.searchParams.get("url");
 
-  if (!encodedPath) {
-    return new Response("Missing 'path' parameter", { status: 400 });
-  }
-
-  if (!encodedUrl) {
-    return new Response("Missing 'url' parameter", { status: 400 });
-  }
-
-
   try {
-    const decodedPath = atob(encodedPath);
-    const backendUrl = `https://be.tastycreative.xyz/media-viewer/stream?path=${encodeURIComponent(encodedPath ? decodedPath : encodeURIComponent(encodedUrl))}`;
+    const decodedPath = encodedPath ? atob(encodedPath) : null;
+    const backendUrl = `https://be.tastycreative.xyz/media-viewer/stream?path=${encodeURIComponent(encodedPath ? decodedPath : encodeURIComponent(encodedUrl || ""))}`;
 
     const fetchRes = await fetch(backendUrl, {
       headers: {
